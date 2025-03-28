dde97a5c89210eb46a7a9fb092f9eb283ee48e99
KatherineJH <ohhomoya@gmail.com>
2025-03-28 12:21:08 +0900

    first commit
    project structure

---
+++ backend/OnlineDeliveryService/src/main/java/com/deliveryTeam/OnlineDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ backend/OnlineDeliveryService/src/test/java/com/deliveryTeam/OnlineDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

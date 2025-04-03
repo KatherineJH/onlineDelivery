e0042cea3f59878d69ba4e1b6438d0a72e7f3405
KatherineJH <ohhomoya@gmail.com>
2025-04-03 10:51:08 +0900

    add carousel
    add carousel

--- frontend/food-delivery-app/src/customerComponent/home/Home.jsx
+++ frontend/food-delivery-app/src/customerComponent/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-// import MultiItemCarousel from "./MultiItemCarousel";
+import MultiItemCarousel from "./MultiItemCarousel";
 // import { useDispatch, useSelector } from "react-redux";
 
 export const Home = () => {
@@ -15,7 +15,7 @@ export const Home = () => {
     <div className="pb-10">
       <section className="banner -z-50 relative flex flex-col justify-center items-center">
         <div className="w-[50vw] z-10 text-center">
-          <p className="w-2x1 lg:text-6xl font-bold z-10 py-5">
+          <p className="text-3xl lg:text-6xl font-bold z-10 py-5">
             Vibrant Fresh Grub
           </p>
           <p className="z-10 text-gray-100 text-xl lg:text-4xl">
@@ -24,6 +24,24 @@ export const Home = () => {
         </div>
         <div className="cover absolute top-0 left-0 right-0"></div>
         <div className="fadeout"></div>
+      </section>
+
+      <section className="p-10 lg:py-10 lg:px-20">
+        <p className="text-2xl font-semibold text-gray-100 py-3 pb-10">
+          Top Meals
+        </p>
+        <MultiItemCarousel />
+      </section>
+      <section className="px-5 lg:px-20 pt-10">
+        <h1 className="text-2xl font-semibold text-gray-400 pb-8">
+          {" "}
+          Tastes of the Month
+        </h1>
+        <div className="flex flex-wrap items-center justify-around gap-5">
+          {/* {restaurant.restaurants.map((item) => (
+            <RestaurantCard item={item} />
+          ))} */}
+        </div>
       </section>
     </div>
   );
--- frontend/food-delivery-app/src/customerComponent/navbar/Navbar.jsx
+++ frontend/food-delivery-app/src/customerComponent/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Badge, Box, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { purple } from "@mui/material/colors";
-import { Person, ShoppingCart } from "@mui/icons-material";
+import { ShoppingCart } from "@mui/icons-material";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -10,6 +10,7 @@ import { useSelector } from "react-redux";
 export const Navbar = () => {
   // const { auth, cart } = useSelector((store) => store);
   const navigate = useNavigate();
+
   const handleClickAvatar = () => {
     // if (auth.user?.role === "ROLE_CUSTOMER") {
     navigate("/my-profile");
@@ -17,35 +18,39 @@ export const Navbar = () => {
     //   navigate("/admin/restaurant");
     // }
   };
+
   return (
     <Box className="px-5 sticky top-0 z-50 py-[1.8rem] bg-[#a885ed] lg:px-20 flex justify-between">
-      <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
-        <li
+      <div className="flex items-center space-x-4 cursor-pointer">
+        <div
           onClick={() => navigate("/")}
           className="logo font-semibold text-white text-2xl"
         >
           BIT FOOD SERVICE
-        </li>
-        <div className="flex items-center space-x-2 lg:space-x-10">
-          <div className="">
-            <IconButton>
-              <SearchIcon sx={{ fontSize: "1.5rem" }} />
-            </IconButton>
-          </div>
-          <div className="">
-            <Avatar
-              onClick={handleClickAvatar}
-              sx={{ bgcolor: "white", color: purple.A700 }}
-            >
-              C
-            </Avatar>
-          </div>
         </div>
-        <div className="">
+      </div>
+
+      <div className="flex items-center space-x-2 lg:space-x-10">
+        <div>
+          <IconButton>
+            <SearchIcon sx={{ fontSize: "1.5rem" }} />
+          </IconButton>
+        </div>
+
+        <div>
+          <Avatar
+            onClick={handleClickAvatar}
+            sx={{ bgcolor: "white", color: purple.A700 }}
+          >
+            C
+          </Avatar>
+        </div>
+
+        <div>
           <IconButton onClick={() => navigate("/cart")}>
             <Badge
               color="secondary"
-              //   badgeContent={cart.cart?.item?.length || 0}
+              // badgeContent={cart.cart?.item?.length || 0} // number of items in my cart
             >
               <ShoppingCart sx={{ fontSize: "1.5rem" }} />
             </Badge>

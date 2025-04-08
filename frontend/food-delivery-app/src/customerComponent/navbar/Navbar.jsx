05a2fbc5adf267ac8fdd37a0b420214f0cf0bd6d
YJOH11 <yjohmail@naver.com>
2025-04-08 15:54:55 +0900

    (in progress) add searchPage+connect productSearchAPI
    +fix registerForm.jsx  'fullName' -> 'userName'

--- frontend/food-delivery-app/src/customerComponent/navbar/Navbar.jsx
+++ frontend/food-delivery-app/src/customerComponent/navbar/Navbar.jsx
@@ -34,7 +34,7 @@ export const Navbar = () => {
 
       <div className="flex items-center space-x-2 lg:space-x-10">
         <div>
-          <IconButton>
+        <IconButton onClick={() => navigate("/search")}>
             <SearchIcon sx={{ fontSize: "1.5rem" }} />
           </IconButton>
         </div>
